c6c0208beb94fea06436d5e6d1253fbfbc0b0593
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-19 00:50:38 +0100

    shows all tweets

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Tweets</h1>
+    @if(count($posts)>0)
+        @foreach($posts as $post)
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"><a href="posts/{{$post->id}}">{{$post->title}}</a></h3>
+                    <small>Tweeted on {{$post->created_at}}</small>
+                </div>
+            </div>
+        @endforeach
+        {{$posts->links()}}
+    @else
+        <p>No posts found</p>
+    @endif
+@endsection
