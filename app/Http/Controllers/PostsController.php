272541d2cf99c6faacde22de88e541585b5bed72
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-19 00:45:38 +0100

    include post model & now controls index, create, store & show

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+// use DB;
 
 class PostsController extends Controller
 {
@@ -13,7 +15,13 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        // $posts = Post::all();
+        // return Post::where('title','Post Two')->get();
+        // $posts = DB::select('SELECT * FROM posts');
+        // $posts = Post::orderBy('title','desc')->take(1)->get();
+        // $posts = Post::orderBy('created_at','desc')->get();
+        $posts = Post::orderBy('created_at','desc')->paginate(10);
+        return view('posts.index')->with('posts',$posts);
     }
 
     /**
@@ -23,7 +31,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -34,7 +42,18 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title'=>'required',
+            'body'=>'required'
+        ]);
+
+        // Create Post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success','Tweeted successfully');
     }
 
     /**
@@ -45,7 +64,9 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        // $post = Post::all();
+        $post = Post::find($id);
+        return view('posts.show')->with('post',$post);
     }
 
     /**
