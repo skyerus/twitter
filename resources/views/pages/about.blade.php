04191499ac84d263f24da51ca69296d9c55f446f
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-18 17:21:22 +0100

    about content

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Vitae, minima? Dolore commodi, vero nulla nam laudantium tenetur aliquam porro sed dignissimos!
+        Porro et debitis iure sequi officia amet, mollitia facere.</p>
+@endsection
