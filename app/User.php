13d81b15bb72833f8f789352f784ea7d46fe9b14
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-20 00:03:29 +0100

    user has many posts

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // A user has many posts
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
