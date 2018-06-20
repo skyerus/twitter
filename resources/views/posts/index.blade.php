27c5fd3e2ee61c71a35abfe4e22d8097ebf356fc
Skye Gill <skyerus95@hotmail.co.uk>
2018-06-20 21:06:54 +0100

    shows cover_image

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,8 +6,16 @@
         @foreach($posts as $post)
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title"><a href="posts/{{$post->id}}">{{$post->title}}</a></h3>
-                    <small>Tweeted on {{$post->created_at}} by {{$post->user->name}}</small>
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4">
+                            <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+                        </div>
+                        <div class="col-md-8 col-sm-8">
+                            <h3 class="card-title"><a href="posts/{{$post->id}}">{{$post->title}}</a></h3>
+                            <small>Tweeted on {{$post->created_at}} by {{$post->user->name}}</small>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         @endforeach
